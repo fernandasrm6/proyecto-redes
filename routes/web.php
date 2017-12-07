c6da610d6a3facc83d0ffaad3604ef4baa0aef6d
Alfonso Strotgen <strotgen@gmail.com>
2017-12-07 02:18:19 +0000

    Ia

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/led/{prendido}', function ($prendido) {
-    //logica guardar bd
-    return $prendido;
+Route::get('/led/', function ($prendido) {
+
+    return 1;
 });
